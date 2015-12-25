d50941fa56abad46159c08b205bd2c258a2bb502
Rudy Reeves <rudyreeves@lclark.edu>
2015-12-25 17:07:42 -0500

    Initial commit for P4: Conference Central

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -8,8 +8,10 @@ from views import app
 
 if __name__ == '__main__':
 
-    test_category_1 = create_category("test_category_1")
-    test_category_2 = create_category("test_category_2")
+    clear_database()
+
+    test_category_1 = create_category("Food")
+    test_category_2 = create_category("Cars")
 
     app.secret_key = "lRYRXEimZGfbt3Q2TpD_6_Kj"
     app.debug = True
